7ff561acf6df3098a88cfaa8e5ee537581ace82f
Gustavo <gustavoraatz@gmail.com>
2024-08-27 17:39:27 -0300

    att

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ namespace Stopwatch
 
             Start(time);
 
-            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaa................a");
+            Console.WriteLine("aaaaaaaaaaaa..........a");
         }
 
         static void Start(int time)
